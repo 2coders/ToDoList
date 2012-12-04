7e3010c92fe209a3c4e7b71fe8cf92891e14e15f
WangLe <longrenle@outlook.com>
2012-12-04 10:29:24 +0800

    Commite PopupWindow Demo.

---
+++ Sample/PopupWindow/Test/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using MessageControl;
+
+namespace Test
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // 构造函数
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            MyMessage.PopupWindow(new WindowsPhoneControl1());
+        }
+
+        
+    }
+}
---
+++ Sample/PopupWindow/Test/WindowsPhoneControl1.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using MessageControl;
+
+namespace Test
+{
+    public partial class WindowsPhoneControl1 : UserControl
+    {
+        public WindowsPhoneControl1()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, RoutedEventArgs e)
+        {
+            MyMessage.HideWindow();
+        }
+    }
+}
