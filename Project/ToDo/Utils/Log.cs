d1990df7f05d9a2e3ee00c02e618282acfaf8621
WangLe <longrenle@gmail.com>
2012-12-26 16:23:55 +0800

    Add Log helper.

---
+++ Project/ToDo/Utils/Log.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+
+namespace ToDo.Utils
+{
+    public class Log
+    {
+        static public void Info(String tag, String message)
+        {
+            Debug.WriteLine("{0}    :{1}", tag, message);
+        }
+
+        static public void Error(String tag, String message)
+        {
+            Debug.WriteLine("{0}    :{1}", tag, message);
+        }
+    }
+}
