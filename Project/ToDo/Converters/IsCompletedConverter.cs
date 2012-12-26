e0578a1e1f8d8ffc881caec76030609c4be11bde
WangLe <longrenle@gmail.com>
2012-12-26 16:26:02 +0800

    Gesture implementation to mark todo item completed, seems a little ugly.

---
+++ Project/ToDo/Converters/IsCompletedConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+
+namespace ToDo.Converters
+{
+    public class IsCompletedConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (bool)value ? "Visible" : "Collapsed";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- Project/ToDo/Views/MainPage.xaml.cs
+++ Project/ToDo/Views/MainPage.xaml.cs
@@ -21,71 +21,6 @@ namespace ToDo
             this.DataContext = App.ViewModel;
         }
 
-        private void TextBlock_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            FrameworkElement tbx = sender as FrameworkElement;
-            StackPanel parent = tbx.Parent as StackPanel;
-
-            if (shownButtons != null)
-            {
-
-                if (shownButtons != parent.Children[1])
-                {
-                    shownButtons.Visibility = System.Windows.Visibility.Collapsed;
-                }
-                else
-                {
-                    AnimationUtils.Fold(shownButtons);
-                }
-            }
-            
-            if ((shownButtons != parent.Children[1]) && ((shownButtons = parent.Children[1]) != null))
-            {
-                //AnimationUtils.Expand(shownButtons);
-                AnimationUtils.Split(todayExpanderView, shownButtons as FrameworkElement);
-            }
-            else
-            {
-                shownButtons = null;
-            }
-        }
-
-        private void TodayTitle_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            //if (todayToDoItemsListBox.Visibility == System.Windows.Visibility.Visible)
-            //{
-            //    todayToDoItemsListBox.Visibility = System.Windows.Visibility.Collapsed;
-            //}
-            //else
-            //{
-            //    todayToDoItemsListBox.Visibility = System.Windows.Visibility.Visible;
-            //}
-        }
-
-        private void TomorrowTitle_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            //if (tomorrowToDoItemsListBox.Visibility == System.Windows.Visibility.Visible)
-            //{
-            //    tomorrowToDoItemsListBox.Visibility = System.Windows.Visibility.Collapsed;
-            //}
-            //else
-            //{
-            //    tomorrowToDoItemsListBox.Visibility = System.Windows.Visibility.Visible;
-            //}
-        }
-
-        private void LaterTitle_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
-        {
-            if (laterToDoItemsListBox.Visibility == System.Windows.Visibility.Visible)
-            {
-                laterToDoItemsListBox.Visibility = System.Windows.Visibility.Collapsed;
-            }
-            else
-            {
-                laterToDoItemsListBox.Visibility = System.Windows.Visibility.Visible;
-            }
-        }
-
         private void ScrollViewer_MouseLeftButtonUp(object sender, System.Windows.Input.MouseButtonEventArgs e)
         {
             e.Handled = true;
@@ -98,7 +33,6 @@ namespace ToDo
             {
                 PopupWindow.ShowWindow(new NoteControl(item));
             }
-            
         }
 
         private void FlagButton_Click(object sender, RoutedEventArgs e)
@@ -128,7 +62,59 @@ namespace ToDo
             if (item != null)
             {
                 App.TodoParams = item;
-                NavigationService.Navigate(new Uri("ReminderPage.xaml", UriKind.Relative));
+                NavigationService.Navigate(new Uri("/Views/ReminderPage.xaml", UriKind.Relative));
+            }
+        }
+
+        private void GestureListener_Flick(object sender, FlickGestureEventArgs e)
+        {
+            if (e.Direction == System.Windows.Controls.Orientation.Horizontal)
+            {
+                ToDoItem item = (sender as FrameworkElement).DataContext as ToDoItem;
+                if (item != null)
+                {
+                    if (e.HorizontalVelocity > 0)//flick to right
+                    {
+                        item.IsCompleted = true;
+                        if (shownButtons != null)
+                        {
+                            shownButtons.Visibility = System.Windows.Visibility.Collapsed;
+                        }
+                    }
+                    else if (e.HorizontalVelocity < 0)//flick to left
+                    {
+                        item.IsCompleted = false;
+                    }
+                }
+            }
+        }
+
+        private void Border_Tap(object sender, System.Windows.Input.GestureEventArgs e)
+        {
+            FrameworkElement tbx = sender as FrameworkElement;
+            StackPanel parent = tbx.Parent as StackPanel;
+
+            if (shownButtons != null)
+            {
+
+                //if (shownButtons != parent.Children[1])
+                {
+                    shownButtons.Visibility = System.Windows.Visibility.Collapsed;
+                }
+                //else
+                //{
+                //    AnimationUtils.Fold(shownButtons);
+                //}
+            }
+
+            if ((shownButtons != parent.Children[1]) && ((shownButtons = parent.Children[1]) != null))
+            {
+                AnimationUtils.Expand(shownButtons);
+                //AnimationUtils.Split(todayExpanderView, shownButtons as FrameworkElement);
+            }
+            else
+            {
+                shownButtons = null;
             }
         }
     }
