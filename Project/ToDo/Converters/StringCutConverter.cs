79b873e1697d06195823d04fb425e9507c0f856e
WangLe <longrenle@outlook.com>
2013-01-23 23:53:40 +0800

    ListItem title string cut.

---
+++ Project/ToDo/Converters/StringCutConverter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace ToDo.Converters
+{
+    public class StringCutConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            string str = value.ToString();
+            int maxLength = 20;
+            if (parameter != null)
+            {
+                maxLength = System.Convert.ToInt32(parameter);
+            }
+            if(str.Length > maxLength)
+            {
+                str = str.Substring(0, maxLength) + "...";
+            }
+            return str;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
